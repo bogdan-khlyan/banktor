08ba1378c9292a67034c85f5f69170f923794802
bogdan-khlyan <es.bogdan.hlyan@gmail.com>
2022-08-13 20:26:55 +0300

    feat: banner and drawer

---
+++ src/js/drawer.js
@@ -0,0 +1,9 @@
+const drawerEl = document.getElementById('drawer')
+
+function clickBurger() {
+    drawerEl.style.right = '0'
+}
+
+function clickCloseDrawer() {
+    drawerEl.style.right = '-380px'
+}
